f467122051bc3ff01b4fd5920a6fcd4f3c4f969d
Filipi Tridharma Ketaren <158836873+filipiKetaren@users.noreply.github.com>
2024-12-01 14:02:12 +0700

    memperbaiki error respon

--- utils/strings_utils.go
+++ utils/strings_utils.go
@@ -2,8 +2,11 @@ package utils
 
 import "strings"
 
-// CapitalizeErrorMessage capitalizes the first letter of an error message
+// CapitalizeErrorMessage capitalizes the first letter of each word in an error message
 func CapitalizeErrorMessage(err error) string {
-	message := err.Error()
-	return strings.ToUpper(string(message[0])) + message[1:]
+	words := strings.Fields(err.Error()) // Pisahkan pesan error menjadi kata-kata
+	for i, word := range words {
+		words[i] = strings.ToUpper(string(word[0])) + word[1:]
+	}
+	return strings.Join(words, " ") // Gabungkan kembali menjadi satu pesan
 }
