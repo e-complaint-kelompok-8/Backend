01a76cd3babe17b4c11b3de31bf7590f7b6de55c
Muhammad Ilhan Mansiz <milhanmansiz1512@gmail.com>
2024-12-01 19:22:17 +0700

    update routes in group api

--- routes/routes.go
+++ routes/routes.go
@@ -21,15 +21,15 @@ func (rc RouteController) RegisterRoutes(e *echo.Echo) {
 	e.POST("/verify-otp", rc.AuthController.VerifyOTPController)
 
 	// Auth Routes for Admin
-	e.POST("/register", rc.AuthAdminController.RegisterAdminHandler)      // Admin registration
+	e.POST("/register", rc.AuthAdminController.RegisterAdminHandler)      
 	e.POST("/login", rc.AuthAdminController.LoginAdminHandler)
 
 	// Grup API
-	api := e.Group("/api/v1")
-	api.GET("/admins", rc.AuthAdminController.GetAllAdminsHandler)        // Get all admins
-	api.GET("/admin/:id", rc.AuthAdminController.GetAdminByIDHandler)     // Get admin by ID
-	api.PUT("/admin", rc.AuthAdminController.UpdateAdminHandler)         // Update admin details
-	api.DELETE("/admin/:id", rc.AuthAdminController.DeleteAdminHandler)  
+	api := e.Group("/admin")
+	api.GET("", rc.AuthAdminController.GetAllAdminsHandler)       
+	api.GET("/:id", rc.AuthAdminController.GetAdminByIDHandler) 
+	api.PUT("", rc.AuthAdminController.UpdateAdminHandler)        
+	api.DELETE("/:id", rc.AuthAdminController.DeleteAdminHandler)  
 
 	// JWT Authentication Middleware for protected routes
 	api.Use(middlewares.JWTMiddleware())
