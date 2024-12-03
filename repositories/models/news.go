e774e041ef9a2a69eadc3b877d959d45eb1bf8c4
Filipi Tridharma Ketaren <158836873+filipiKetaren@users.noreply.github.com>
2024-12-03 14:01:34 +0700

    meyiapkan request/response models

--- repositories/models/news.go
+++ repositories/models/news.go
@@ -1,12 +1,35 @@
 package models
 
-import "time"
+import (
+	"capstone/entities"
+	"time"
+)
 
 // News struct
 type News struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement"`
+	ID        int       `gorm:"primaryKey;autoIncrement"`
 	Title     string    `gorm:"type:varchar(255);not null"`
 	Content   string    `gorm:"type:text;not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+func FromEntitiesNews(news entities.News) News {
+	return News{
+		ID:        news.ID,
+		Title:     news.Title,
+		Content:   news.Content,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
+
+func (news News) ToEntities() entities.News {
+	return entities.News{
+		ID:        news.ID,
+		Title:     news.Title,
+		Content:   news.Content,
+		CreatedAt: news.CreatedAt,
+		UpdatedAt: news.UpdatedAt,
+	}
 }
